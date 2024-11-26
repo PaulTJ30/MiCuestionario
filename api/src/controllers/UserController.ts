0bd3834d85d90619df7dbae93bffb1ce15552ba9
PaulTJ30 <pultj30@gmail.com>
2024-11-25 21:35:02 -0600

    Cambios de autenticación de usuario

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { UserModel } from "../modules/UsersModel";
+import jwt from "jsonwebtoken"
 
 export const registerUsers = async (req: Request, res: Response):
     Promise<any> => {
@@ -7,8 +8,8 @@ export const registerUsers = async (req: Request, res: Response):
 
         //Primero validar que los datos que necesitamos existen
         const name = req.body.name
-        const email = req.body.email
         const lastNames = req.body.lastNames
+        const email = req.body.email
         const password = req.body.password
         const rol = req.body.rol
         //Administradores no pueden crear clientes
@@ -17,7 +18,7 @@ export const registerUsers = async (req: Request, res: Response):
         }
         if (!name || !email || !lastNames || !password || !rol) {
             return res.status(400).json({
-                msg: "Faltan datos para crear un ususarios"
+                msg: "Faltan datos para crear un usuario"
             })
         }
         // Validar que el usuario sea administrador si el usuario a crear es administrador
@@ -27,20 +28,52 @@ export const registerUsers = async (req: Request, res: Response):
             })
         }
 
-        await UserModel.create({
+        const user = await UserModel.create({
             name,
             lastNames,
             email,
             password,
             rol
         })
+        const token = jwt.sign(JSON.stringify(user), "shhhh")
 
         return res.status(200).json({
-            msg: "Usuario registrado con exito"
+            msg: "Usuario registrado con exito", token
         })
     } catch (error) {
         return res.status(500).json({
             msg: "Hubo un error al crear el usuario"
         })
     }
+
+}
+
+export const singin = async (req: Request, res: Response): Promise<any> => {
+
+    // Correo y contraseña
+    // Veirificar que el usuario existe
+    // Si no existe devuelven error
+    try {
+        const { email, password } = req.body
+        if (!email || password) {
+            return res.status(400).json({
+                msg: "Correo y contraseña necesarios"
+            })
+        }
+        const user = await UserModel.findOne({ correo: req.body.email, password: req.body.password })
+        if (!user) {
+            return res.status(404).json({
+                msg: "Usuario no existente"
+            })
+        }
+        const token = jwt.sign(JSON.stringify(user), "shhhh")
+        return res.status(200).json({
+            msg: `Este es tu token `, token
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: "Hubo un erro al encontrar el usuario"
+        })
+    }
+
 }
